207710b89546a9b72484fa13ca79e03f6ac61c39
Olivier Roger <olivier.roger@gmail.com>
2017-07-30 03:09:53 +0200

    Add documentation link

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
+# See documentation 
+# @ https://docs.travis-ci.com/user/deployment/custom/
+# @ http://ajaykarwal.com/deploying-jekyll-using-travis-ci/
 ncftpput -R -v -u $FTP_USER -p $FTP_PASSWORD ftp.comprendre-steem.fr / public/*
